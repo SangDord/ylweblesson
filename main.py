d3af3b3cb022eb08abb6b8f58d7475be4bb5ca5a
sangdord <sangdord07@gmail.com>
2024-03-27 14:41:00 +0300

    Изображение Марса

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, url_for
 
 app = Flask(__name__)
 
@@ -23,5 +23,25 @@ def promotion():
     return '</br>'.join(promotion_text)
 
 
+@app.route('/image_mars')
+def image_mars():
+    return f'''<!DOCTYPE html>
+                <html lang="en">
+                    <head>
+                        <meta charset="utf-8">
+                        <title>Привет, Марс!</title>
+                    </head>
+                    <body>
+                        <h1>Жди нас, Марс!</h1>
+                        <figure>
+                            <img src="{url_for('static', filename='img/mars.png')}">
+                            <figcaption>
+                                Вот она какая, красная планета.
+                            </figcaption>
+                        </figure>
+                    </body>
+                </html>'''
+                
+                
 if __name__ == "__main__":
     app.run(port=8080, host='127.0.0.1')
