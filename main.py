31de1ea74d484641c8099d32d4bc5447ed43e39f
sangdord <sangdord07@gmail.com>
2024-04-04 07:15:49 +0300

    Список профессий

--- main.py
+++ main.py
@@ -14,5 +14,25 @@ def training_prof(prof):
     return render_template('training_prof.html', prof=prof)
 
 
+@app.route('/list_prof/<list>')
+def list_prof(list):
+    prof_list = ['инженер-исследователь',
+                 'пилот',
+                 'строитель',
+                 'экзобиолог',
+                 'врач',
+                 'инженер по терраформарованию',
+                 'климатолог',
+                 'специалист по радиационной защите',
+                 'астрогеолог',
+                 'инженер жизнеобеспечения',
+                 'метеоролог',
+                 'оператор марсохода',
+                 'киберинженер',
+                 'штурман',
+                 'пилот дронов']
+    return render_template('list_prof.html', list=list, prof_list=prof_list)
+
+
 if __name__ == "__main__":
     app.run(port=8080, host='127.0.0.1')
