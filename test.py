c32337f0a4d375062d18911a39668ff6d0e90a7d
sangdord <sangdord07@gmail.com>
2024-04-18 02:52:34 +0300

    Тестирование ответа сервера

--- test.py
+++ test.py
@@ -2,6 +2,14 @@ from requests import get
 from pprint import pprint
 
 
-pprint(get('http://localhost:8080/api/jobs/5').json())
-pprint(get('http://localhost:8080/api/jobs/q').json())
+print('Получение всех работ')
+pprint(get('http://localhost:8080/api/jobs').json())
+print()
+print('Корректное получение одной работы (id=1)')
 pprint(get('http://localhost:8080/api/jobs/1').json())
+print()
+print('Ошибочный запрос на получение одной работы (id=99)')
+pprint(get('http://localhost:8080/api/jobs/99').json())
+print()
+print('Ошибочный запрос на получение одной работы (id="stroka")')
+pprint(get('http://localhost:8080/api/jobs/stroka').json())
